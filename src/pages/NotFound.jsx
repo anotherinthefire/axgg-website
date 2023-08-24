090bc8a1a10021631c70116a97c32c5d45c43a6a
anotherinthefire <rongodfreyultra@gmail.com>
2023-08-24 20:33:47 +0800

    not found cc

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom"
 
 const NotFound = () => {
   return (
-    <div>
+    <div className="flex">
         NotFound
         <Link to="/" >Home</Link>
     </div>
