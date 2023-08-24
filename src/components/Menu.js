bfdcdb7e562e8bdcec4cea460204dd7b2014cff2
anotherinthefire <rongodfreyultra@gmail.com>
2023-08-25 05:32:26 +0800

    nav and footer items

---
+++ src/components/Menu.js
@@ -0,0 +1,9 @@
+const Menu = [
+    { title: "Home", path: "" },
+    { title: "Products", path: "category" },
+    { title: "Size Chart", path: "size-chart" },
+    { title: "FAQs", path: "faqs" },
+    { title: "About", path: "about-us" }
+  ]
+
+export default Menu
