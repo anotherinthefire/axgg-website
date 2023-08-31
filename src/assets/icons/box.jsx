f81ac040f24ed3abb109ac59cd779f424e4f733f
anotherinthefire <rongodfreyultra@gmail.com>
2023-08-31 18:49:35 +0800

    carousel icons

---
+++ src/assets/icons/box.jsx
@@ -0,0 +1,30 @@
+const Box = () => {
+    return (
+        <>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
+                <path fillRule="evenodd" d="M4.5 7.5a3 3 0 013-3h9a3 3 0 013 3v9a3 3 0 01-3 3h-9a3 3 0 01-3-3v-9z" clipRule="evenodd" />
+            </svg>
+
+            {/* 
+            
+              const goToSlide = (slideIndex) => {
+    setCurrentIndex(slideIndex);
+  };
+  
+            <div className='flex top-4 justify-center py-2'>
+          {slides.map((slide, slideIndex) => (
+            <div
+              key={slideIndex}
+              onClick={() => goToSlide(slideIndex)}
+              className='text-2xl cursor-pointer'
+            >
+              <Box />
+            </div>
+          ))}
+        </div>
+            */}
+        </>
+    )
+}
+
+export default Box
